691f7d1c9d0b50605f61ca7d3493d36c6873ce31
Tommy Sundling <tosundli@microsoft.com>
2024-02-19 11:07:21 +0100

    Create script for configuring MSFT Defender for external subs

--- Scripts/msft-configure-external-subscriptions.ps1
+++ Scripts/msft-configure-external-subscriptions.ps1
@@ -1,23 +1,46 @@
 
 $tenantID = "<tenant ID>"
 $subscriptionIDs = @("<subscription ID 1>", "<subscription ID 2>")
-
+$securityContactEmail = "<security contact email>"
 
 
 # Connect to your Azure account
-Connect-AzAccount --tenantid $tenantID
+Connect-AzAccount -TenantId $tenantID -Subscription $subscriptionIDs[0] | Out-Null
 
-# Set your default subscription
-Get-AzSubscription
-Set-AzContext -Subscription "<subscription ID>"
 
-# Register the Microsoft.Security resource provider
-Register-AzResourceProvider -ProviderNamespace 'Microsoft.Security'
+# Configure Microsoft Defender for Cloud for each subscription
+foreach ($subscriptionID in $subscriptionIDs) {
+  Write-Host "Configuring subscription: $subscriptionID..."
+  Set-AzContext -Subscription $subscriptionID -Tenant $tenantID | Out-Null
+  # Register the Microsoft.Security resource provider
+  Write-Host "Register security provider..." -NoNewline
+  Register-AzResourceProvider -ProviderNamespace 'Microsoft.Security' | Out-Null
+  Write-Host "...done."
 
-# Define the security contact details
-Set-AzSecurityContact `
-  -Name "SecurityContact1" `
-  -Email "security@yourdomain.com" `
-  -Phone "555-1234" `
-  -AlertAdmin `
-  -NotifyOnAlert
+  # Define the URI
+  $uri = "https://management.azure.com/subscriptions/" + $subscriptionID + "/providers/Microsoft.Security/securityContacts/default?api-version=2020-01-01-preview"
+
+  # Define the body
+  $body = @{
+      "properties" = @{
+          "emails" = $securityContactEmail
+          "notificationsByRole" = @{
+              "state" = "On"
+              "roles" = @("ServiceAdmin","Owner")
+          }
+          "alertNotifications" = @{
+              "state" = "On"
+              "minimalSeverity" = "Medium"
+          }
+          "phone" = ""
+      }
+  } | ConvertTo-Json -Depth 5
+
+  # Send the PUT request
+  $response = Invoke-RestMethod -Uri $uri -Method Put -Body $body -ContentType "application/json" -Headers @{Authorization="Bearer $(az account get-access-token --query accessToken -o tsv)"}
+
+  # Output the response
+  $response
+
+  Write-Host "Subscription done."
+}
