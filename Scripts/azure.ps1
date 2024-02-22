f4800eb671c868617a7bd2047669a42217d64662
Tommy Sundling <tosundli@microsoft.com>
2024-02-22 11:09:53 +0100

    Added convenience script for working with sub

--- Scripts/azure.ps1
+++ Scripts/azure.ps1
@@ -12,3 +12,18 @@ Get-AzResourceProvider -ListAvailable |
     Where-Object { $_.Locations -contains 'Sweden Central' } | 
     Select-Object -Property ProviderNamespace -ExpandProperty ResourceTypes | 
     Format-Table -Property ProviderNamespace, ResourceTypeName
+
+
+### DO WORK IN EACH SUBSCRIPTION
+
+# Get list of all subscriptions in a tenant
+$tenantId = "<tenant ID>"
+$subscriptions = Get-AzSubscription -TenantId $tenantId | Select-Object -Property Name, Id
+
+# Do work in each subscription
+foreach ($subscription in $subscriptions) {
+    # Set the subscription context
+    Set-AzContext -Subscription $subscription.Id -TenantId $tenantId | Out-Null
+    Write-Host "Working in subscription: $($subscription.Name)"
+    # Do work here
+}
